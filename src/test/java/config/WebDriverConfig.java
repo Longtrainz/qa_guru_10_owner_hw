fda0d260f94514ca430954c187980fe09a76dc93
Vladyslav_Bukach <vladyslav.bukach@mckesson.com>
2021-03-10 19:40:41 +0200

    updated parameter

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 
-@Config.Sources("classpath:${environment}.properties")
+@Config.Sources("classpath:${env}.properties")
 public interface WebDriverConfig extends Config {
 
 	@Key("browser")
